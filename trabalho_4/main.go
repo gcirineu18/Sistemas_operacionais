06cc0bf153a9661108da81d7ab1adcd2e7665d73
Guilherme Barbosa <guilherme.cirineu@great.local.ufc.br>
2025-12-10 16:52:24 -0300

    refactor: parsing

--- trabalho_4/main.go
+++ trabalho_4/main.go
@@ -4,16 +4,19 @@ import
 (
   "fmt"
   "os"
+  "strconv"
+  "bufio"
+   "log"
 )
 
 type Pager struct{
 	alg string
-	trace  string
-	frames uint8
+	trace []int
+	frame int
 }
 
 
-func parseCommands(){
+func parseCommands() *Pager{
 	argsMap := make(map[string]int)
 	argsMap["algo"] = 0
 	argsMap["frames"] = 0
@@ -37,18 +40,69 @@ func parseCommands(){
 		}
 	}
 
-	getAlgo(os.Args[argsMap["algo"]])
-	getFrame(os.Args[argsMap["frames"]])
-	getTrace(os.Args[argsMap["trace"]])
+	alg:=getAlgo(os.Args[argsMap["algo"]])
+	frame:=getFrame(os.Args[argsMap["frames"]])
+	trace:=getTrace(os.Args[argsMap["trace"]])
+
+	return &Pager{alg: alg, frame: frame, trace: trace}
 }
 
-func getAlgo(alg string){
-
+func getAlgo(alg string) string{
+	aux:= 0 
+	switch alg {
+		case "fifo", "lru", "otimo", "sc", "clock", "nru", "lfu", "mfu":
+			aux = 1
+	}
+		
+	if aux == 0 {
+		fmt.Printf("Nome de algoritmo inválido: %s\n", alg)
+		os.Exit(1)
+	}		
+	return alg	
 }
-func getFrame(frame string){
 
+
+func getFrame(frame string) int{
+
+  num, err := strconv.Atoi(frame)	
+  if err != nil || num <= 0 {
+	fmt.Println("O número de frames é inválido.")
+	os.Exit(1)
+  }
+  return num
 }
-func getTrace(trace string){
+func getTrace(trace string) []int{
+  file, err := os.Open(trace)
+
+  if  err != nil {
+	log.Fatal(err)
+  }
+
+  defer file.Close()
+  
+  var pagesInput []int
+  scanner:= bufio.NewScanner(file)
+  for scanner.Scan(){
+
+	linha := scanner.Text() // linha inteira como string
+
+    num, err := strconv.Atoi(linha)
+    if err != nil {
+        log.Printf("%v", err)
+        continue
+    }
+
+    fmt.Printf("num: %d, tipo: %[1]T\n", num)
+
+	 pagesInput = append(pagesInput, num)
+
+  }
+
+  if err := scanner.Err(); err != nil{
+	log.Fatal(err)
+  }
+
+  return pagesInput
 
 }
 
@@ -56,7 +110,8 @@ func main(){
 
 	if len(os.Args) < 7  || len(os.Args) > 8 {
 		fmt.Printf("Número de argumentos inválidos. O Padrão deve ser:\n" +
-		"go run --algo <ALGO> --frames <N> --trace <arquivo> [--verbose]\n")
+		"go run main.go --algo <ALGO> --frames <N> --trace <arquivo> [--verbose]\n")
+		os.Exit(1)
 	}
 
 	parseCommands()
